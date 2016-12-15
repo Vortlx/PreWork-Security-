ed6ef2fdec3f86111aabcaaa391fd75723dc8463
LebedevAS <JKDSGJKSDJG@gmail.com>
2016-12-15 14:36:10 +0400

    Was added removing errors messages if field is correct in changePassword page

--- src/main/webapp/javascript/validationPassword.js
+++ src/main/webapp/javascript/validationPassword.js
@@ -5,21 +5,30 @@ function validPassword(){
     var oldPassword = document.getElementById("oldPassword");
     var newPassword = document.getElementById("newPassword");
 
+    clearErrorMessage();
     if(oldPassword.value == ""){
         var message = document.getElementById("incorrOldPass");
-        message.innerHTML = "   Please enter old password";
+        message.innerHTML = "Please enter old password";
         return false;
-    }else if(oldPassword.value.length <= 3){
+    }else if(oldPassword.value.length < 3){
         var message = document.getElementById("incorrOldPass");
-        message.innerHTML = "   Password must be not less then 4 character";
+        message.innerHTML = "Password must be not less then 3 character";
         return false;
     }else if(newPassword.value == ""){
         var message = document.getElementById("incorrNewPass");
-        message.innerHTML = "   Password not be empty";
+        message.innerHTML = "Password not be empty";
         return false;
-    }else if(newPassword.value.length <= 3){
+    }else if(newPassword.value.length < 3){
         var message = document.getElementById("incorrNewPass");
-        message.innerHTML = "   Password must be not less then 4 character";
+        message.innerHTML = "Password must be not less then 3 character";
         return false;
     }
-}
+};
+
+function clearErrorMessage(){
+    var oldPass = document.getElementById("incorrOldPass");
+    var newPass = document.getElementById("incorrNewPass");
+
+    oldPass.innerHTML = "";
+    newPass.innerHTML = "";
+};
