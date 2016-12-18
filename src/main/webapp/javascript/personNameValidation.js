0b36f7c740e03fba67e23b15f6aad7bccd246076
Alexander Lebedev <jkdsgjksdjg@gmail.com>
2016-12-18 18:41:03 +0400

    personNameValidation.js was rewritten with JQuery

--- src/main/webapp/javascript/personNameValidation.js
+++ src/main/webapp/javascript/personNameValidation.js
@@ -2,38 +2,34 @@
  * Created by lebedevas on 15.12.2016.
  */
 function personIncorrectNameValidation(){
-    var personName = document.getElementById("personName");
-    var personFamilyName = document.getElementById("personFamilyName");
+    var personName = $("#personName");
+    var personFamilyName = $("#personFamilyName");
 
     var regExpr = /.*[0-9]+.*/i;
 
     clearErrorMessage();
 
-    if(regExpr.test(personName.value)){
-        var message = document.getElementById("incorrName");
-        message.innerHTML = "Was entered incorrect name";
+    if(regExpr.test(personName.val())){
+        $("#incorrName").html("Was entered incorrect name");
         return false;
-    }else if(regExpr.test(personFamilyName.value)){
-        var message = document.getElementById("incorrFamilyName");
-        message.innerHTML = "Was entered incorrect family name";
+    }else if(regExpr.test(personFamilyName.val())){
+        $("#incorrFamilyName").html("Was entered incorrect family name");
         return false;
     }
     return true;
 };
 
 function personEmptyNameValidation(){
-    var personName = document.getElementById("personName");
-    var personFamilyName = document.getElementById("personFamilyName");
+    var personName = $("#personName");
+    var personFamilyName = $("#personFamilyName");
 
     clearErrorMessage();
 
-    if(personName.value == ""){
-        var message = document.getElementById("incorrName");
-        message.innerHTML = "Please enter name";
+    if(personName.val() == ""){
+        $("#incorrName").html("Please enter name");
         return false;
-    }else if(personFamilyName.value == ""){
-        var message = document.getElementById("incorrFamilyName");
-        message.innerHTML = "Please enter family name";
+    }else if(personFamilyName.val() == ""){
+        $("#incorrFamilyName").html("Please enter family name");
         return false;
     }
     return true;
@@ -41,12 +37,9 @@ function personEmptyNameValidation(){
 
 function allPersonValidation(){
     return personEmptyNameValidation() && personIncorrectNameValidation();
-}
+};
 
 function clearErrorMessage(){
-    var messageName = document.getElementById("incorrName");
-    var messageFamilyName = document.getElementById("incorrFamilyName");
-
-    messageName.innerHTML = "";
-    messageFamilyName.innerHTML = "";
+    $("#incorrName").html("");
+    $("#incorrFamilyName").html("");
 };
