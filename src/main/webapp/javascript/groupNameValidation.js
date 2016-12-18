edadff8f362ff5558a8101695c6c581f5e782a52
Alexander Lebedev <jkdsgjksdjg@gmail.com>
2016-12-18 18:35:51 +0400

    groupNameValidation.js was rewritten with JQuery

--- src/main/webapp/javascript/groupNameValidation.js
+++ src/main/webapp/javascript/groupNameValidation.js
@@ -2,24 +2,20 @@
  * Created by lebedevas on 15.12.2016.
  */
 function groupNameValidation(){
-    var groupName = document.getElementById("groupName");
+    var groupName = $("#groupName");
 
     var regExpr = /.*[a-zA-Z]+.*/;
 
     clearErrorMessage();
-    if(groupName.value == ""){
-        var message = document.getElementById("incorrGroupName");
-        message.innerHTML = "Please enter group name";
+    if(groupName.val() == ""){
+        $("#incorrGroupName").html("Please enter group name");
         return false;
-    }else if(regExpr.test(groupName.value)){
-        var message = document.getElementById("incorrGroupName");
-        message.innerHTML = "Was entered incorrect group name";
+    }else if(regExpr.test(groupName.val())){
+        $("#incorrGroupName").html("Was entered incorrect group name");
         return false;
     }
+    
+    function clearErrorMessage(){
+        $("#incorrGroupName").html("");
+    };
 };
-
-function clearErrorMessage(){
-    var message = document.getElementById("incorrGroupName");
-
-    message.innerHTML = "";
-}
